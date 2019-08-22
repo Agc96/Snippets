a3118a81a5192f05763c29f824d44a59b1097826
Agc96 <anthony.gutierrez@pucp.pe>
2019-08-22 15:39:37 -0500

    Container tracking

---
+++ tracking/unused.py
@@ -0,0 +1,27 @@
+"""
+TeamViewer:
+-  973622373 cubil
+- 1227142461 anita
+- 1235296015 anthony
+"""
+
+{"container":{"$regex":"(CADU|CNIU|ENAU|GRIU|HASU|KHJU|KHLU|SEAU|SUDU)"}}
+
+assert movements[2]["date"]             = datetime.datetime("")
+assert movements[2]["location"]         = "HUANGPU, GUANGDONG (CN)"
+assert movements[2]["latitude"]         = 23.1824507
+assert movements[2]["longitude"]        = 113.4760861
+assert movements[2]["status"]           = "Discharged (FCL)"
+assert movements[2]["transport_type"]   = "Vessel"
+assert movements[2]["transport_ship"]   = "SHUN FENG 2018"
+assert movements[2]["transport_voyage"] = "52010190409"
+assert movements[2]["estimated"]        = False
+
+"""
+https://www.coursera.org/learn/machine-learning
+https://en.wikipedia.org/wiki/Queueing_theory
+"""
+
+Primero analizamos:
+https://scikit-learn.org/stable/modules/generated/sklearn.svm.SVR.html#sklearn.svm.SVR
+https://scikit-learn.org/stable/modules/generated/sklearn.svm.NuSVR.html#sklearn.svm.NuSVR
